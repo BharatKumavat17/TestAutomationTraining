5aa18153709f6066350a065325cceccae4d042cf
BharatKumavat17 <kumavatbharat17@gmail.com>
2023-07-05 23:32:18 +0530

    Hover Page Commit

--- src/main/java/pages/HoversPage.java
+++ src/main/java/pages/HoversPage.java
@@ -14,19 +14,26 @@ public class HoversPage {
         this.driver = driver;
     }
 
+    /**
+     *
+     * @param index starts at 1
+     */
     public FigureCaption hoverOverFigure(int index)
     {
+        //figurecaption return multiple element
         WebElement figure = driver.findElements(figureBox).get(index-1); //here this get 3 element and select 1st element using "get()" method
 
         Actions actions = new Actions(driver);
         actions.moveToElement(figure).perform();
         return new FigureCaption(figure.findElement(boxCaption));
     }
-
+    //New approach to handle found Element fields
     public class FigureCaption{
+        //accept element in caption
         private WebElement caption;
         private By header = By.tagName("h5");
         private By link = By.tagName("a");
+        //this will execute
         public FigureCaption(WebElement caption)
         {
             this.caption = caption;
@@ -34,6 +41,7 @@ public class HoversPage {
 
         public boolean isCaptionDisplayed()
         {
+            //Here caption added so anything that needs to be find is inside caption elemebnt
             return caption.isDisplayed();
         }
         public String getTitle()
